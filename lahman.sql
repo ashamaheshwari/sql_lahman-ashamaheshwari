3e722163985faed847cd761aeccccca1e9f63255
ashamaheshwari <ashamaheshwari163@gmail.com>
2023-02-09 10:40:11 -0600

    Working on Question 3

--- lahman.sql
+++ lahman.sql
@@ -1,17 +1,40 @@
-select *
-FROM people
 
--- Find all players in the database who played at Vanderbilt University. Create a list showing each player's first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
-WITH salary AS (SELECT playerid, salary
-			   FROM salary)
-SELECT DISTINCT p.playerid, schoolid, namefirst, namelast
-FROM people AS p
-LEFT JOIN Collegeplaying as c
-ON p.playerid = c.playerid 
-LEFT JOIN salary USING playerid
-WHERE c.schoolid = 'vandy'
+-- QUES 1.
+-- Find all players in the database who played at Vanderbilt University. Create a list showing each player's 
+--first and last names as well as the total salary they earned in the major leagues. 
+--Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
--- Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+WITH vanderbilt_player AS (SELECT 
+						      DISTINCT p.playerid, 
+						      schoolid, 
+						      namefirst, 
+						      namelast
+                           FROM people AS p
+                           LEFT JOIN Collegeplaying as c
+                           USING(playerid)
+                           WHERE c.schoolid = 'vandy') 
+SELECT namefirst, 
+	   namelast, 
+	   SUM(salary)::numeric::money AS total_salary, 
+	   COUNT(DISTINCT yearid) AS total_years_played
+FROM salaries AS s
+	 INNER JOIN vanderbilt_player
+	 USING(playerid)
+GROUP BY playerid, namefirst, namelast
+ORDER BY total_salary DESC;
+
+
+-- Using the fielding table, group players into three groups based on their position: label players with position OF as 
+--"Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". 
+--Determine the number of putouts made by each of these three groups in 2016.
+
+SELECT SUM(po), 
+CASE WHEN pos = 'OF' THEN 'Outfield'
+     WHEN pos IN('SS', '1B', '2B', '3B' ) THEN 'Infield'
+	 WHEN pos IN('P', 'C') THEN 'Battery' END AS group_position
+FROM fielding
+WHERE yearid = '2016'
+GROUP BY group_position
 
 -- Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends? (Hint: For this question, you might find it helpful to look at the generate_series function (https://www.postgresql.org/docs/9.1/functions-srf.html). If you want to see an example of this in action, check out this DataCamp video: https://campus.datacamp.com/courses/exploratory-data-analysis-in-sql/summarizing-and-aggregating-numeric-data?ex=6)
 
