a34e340aa7532e82363f963a8e926665defcda39
ashamaheshwari <ashamaheshwari163@gmail.com>
2023-02-09 21:26:16 -0600

    Working on question 4

--- lahman.sql
+++ lahman.sql
@@ -39,7 +39,23 @@ LEFT JOIN earnings
 USING(playerid)
 ORDER BY big_league_pay DESC;
 
-
+SELECT namefirst, 
+	   namelast, 
+	   SUM(salary)::numeric::money AS total_salary, 
+	   COUNT(DISTINCT yearid) AS years_played
+FROM people
+	 INNER JOIN salaries
+	 USING(playerid)
+WHERE playerid IN (
+SELECT 
+	playerid
+	FROM collegeplaying 
+		LEFT JOIN schools
+		USING(schoolid)
+	WHERE schoolid = 'vandy'
+)
+GROUP BY playerid, namefirst, namelast
+ORDER BY total_salary DESC;
 
 --Ques 2
 -- Using the fielding table, group players into three groups based on their position: label players with position OF as 
@@ -52,7 +68,7 @@ CASE WHEN pos = 'OF' THEN 'Outfield'
 	 WHEN pos IN('P', 'C') THEN 'Battery' END AS group_position
 FROM fielding
 WHERE yearid = '2016'
-GROUP BY group_position
+GROUP BY group_position;
 
 --Ques 3
 -- Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places.
@@ -60,16 +76,36 @@ GROUP BY group_position
 --(Hint: For this question, you might find it helpful to look at the generate_series function (https://www.postgresql.org/docs/9.1/functions-srf.html). 
 --If you want to see an example of this in action, check out this DataCamp video: https://campus.datacamp.com/courses/exploratory-data-analysis-in-sql/summarizing-and-aggregating-numeric-data?ex=6)
 
-select DISTINCT yearid
-from batting
-ORDER BY yearid DESC
 
-
-SELECT *
-FROM generate_series(1920,2010,10 )
+WITH generate_series AS (SELECT * FROM
+					 generate_series(1920,2010,10 ))
+					 
+SELECT  generate_series, 
+        ROUND(SUM(so)*1.0/SUM(g), 2) AS avg_strikeouts,
+		ROUND(SUM(hr)*1.0/SUM(g), 2) AS avg_homeruns
+FROM pitching
+INNER JOIN generate_series
+ON generate_series+1 <= yearid AND generate_series+10 >= yearid
+WHERE yearid >= 1920
+GROUP BY generate_series
+ORDER BY generate_series DESC
+	
+-- Number of games	
+WITH generate_series AS (SELECT * FROM
+					 generate_series(1920,2010,10 ))
+SELECT generate_series, SUM(g)	
+FROM pitching
+INNER JOIN generate_series
+ON generate_series+1 <= yearid AND generate_series+10 >= yearid
+GROUP BY generate_series
+ORDER BY generate_series DESC
 
 --Ques 4
--- Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases. Report the players' names, number of stolen bases, number of attempts, and stolen base percentage.
+-- Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of 
+--stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.)
+--Consider only players who attempted at least 20 stolen bases. Report the players' names, number of stolen bases, number of 
+--attempts, and stolen base percentage.
+
 
 
 --Ques 5
